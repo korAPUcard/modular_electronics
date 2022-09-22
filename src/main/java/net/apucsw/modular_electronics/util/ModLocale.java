8f8601ab516204e7f9405d7211aae6afe3eb5d60
korAPUcard <tmdrnr1157@gmail.com>
2022-09-22 19:55:12 +0900

    문법 레전드

--- src/main/java/net/apucsw/modular_electronics/util/ModLocale.java
+++ src/main/java/net/apucsw/modular_electronics/util/ModLocale.java
@@ -2,6 +2,6 @@ package net.apucsw.modular_electronics.util;
 
 public class ModLocale {
     // The codes used only for development testing yet.
-    // Because i still doesn't have any API.
+    // Because I still don't have any API.
     // Maybe the mod API will be developed later.
 }
