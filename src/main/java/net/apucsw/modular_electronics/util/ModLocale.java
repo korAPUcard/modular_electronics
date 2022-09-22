2a898e70738e1eb93b0a08f6c3b28409acff8044
korAPUcard <tmdrnr1157@gmail.com>
2022-09-22 19:53:32 +0900

    Update ModLocale.java

--- src/main/java/net/apucsw/modular_electronics/util/ModLocale.java
+++ src/main/java/net/apucsw/modular_electronics/util/ModLocale.java
@@ -1,4 +1,7 @@
 package net.apucsw.modular_electronics.util;
 
 public class ModLocale {
+    // The codes used only for development testing yet.
+    // Because i still doesn't have any API.
+    // Maybe the mod API will be developed later.
 }
