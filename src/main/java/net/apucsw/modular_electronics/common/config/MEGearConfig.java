6bf96ab5d27ed98c7ae3fe381368c15c7ee21b46
korAPUcard <tmdrnr1157@gmail.com>
2022-09-25 06:40:39 +0900

    Update MEGearConfig.java

--- src/main/java/net/apucsw/modular_electronics/common/config/MEGearConfig.java
+++ src/main/java/net/apucsw/modular_electronics/common/config/MEGearConfig.java
@@ -1,8 +1,5 @@
 package net.apucsw.modular_electronics.common.config;
 
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import mekanism.api.math.FloatingLong;
 import mekanism.common.config.BaseMekanismConfig;
 import mekanism.common.config.value.CachedFloatingLongValue;
